405ab3575a751a8d92ab22813f5a4f6fb41a78e5
christiangos <55211236+christiangos@users.noreply.github.com>
2023-08-30 15:31:57 +0200

    update
    changed order of user inputs

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 x1 = input("Enter your first number: ")
-x2 = input("Enter your second number: ")
 operation = input ("Enter the operation you want to perform (+ , - , * , /)")
+x2 = input("Enter your second number: ")
+
 
 x1F = float(x1)
 x2F = float(x2)
